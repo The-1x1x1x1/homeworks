2a644f69ac3643f502005e3317dc8bff83dc2b7b
1x1x1x1 <nullandvoid186@gmail.com>
2026-01-12 20:56:12 +0000

    Add files via upload

---
+++ calend.py
@@ -0,0 +1,4 @@
+import calendar 
+
+for x in calendar.month_name:
+    print(x)
---
+++ sincostan.py
@@ -0,0 +1,10 @@
+import math
+
+n=int(input("enter angle: "))
+s=math.sin(n)
+c=math.cos(n)
+t=math.tan(n)
+
+print("sin is: ",s)
+print("cos is: ",c)
+print("tan is: ",t)
